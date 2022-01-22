4d6e6021ee47e40b10b101e451102f7ceaf01275
Carlos Brito <carlos.brito524@gmail.com>
2022-01-22 20:58:04 +0100

    Add missing include <cstring>

--- src/RodinExternal/MMG/Mesh2D.cpp
+++ src/RodinExternal/MMG/Mesh2D.cpp
@@ -4,8 +4,9 @@
  *       (See accompanying file LICENSE or copy at
  *          https://www.boost.org/LICENSE_1_0.txt)
  */
-#include <fstream>
 #include <limits>
+#include <cstring>
+#include <fstream>
 
 #include <libmmgcommon.h>
 #include <mmg2d/mmg2d.h>
