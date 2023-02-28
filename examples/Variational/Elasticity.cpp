c4cbc4d9ed1af56b56231759c42e9df467064ec0
Carlos Brito <carlos.brito524@gmail.com>
2023-02-28 09:55:40 +0100

    add alternative

--- examples/Variational/Elasticity.cpp
+++ examples/Variational/Elasticity.cpp
@@ -7,7 +7,7 @@
 #include <Rodin/Solver.h>
 #include <Rodin/Geometry.h>
 #include <Rodin/Variational.h>
-#include <Rodin/Variational/Traits.h>
+#include <Rodin/Variational/LinearElasticity.h>>
 
 using namespace Rodin;
 using namespace Rodin::Geometry;
@@ -30,7 +30,7 @@ int main(int argc, char** argv)
   H1 sh(Omega);
 
   // Lamé coefficients
-  Scalar mu = 0.3846, lambda = 0.5769;
+  Scalar lambda = 0.5769, mu = 0.3846;
 
   // Pull force
   VectorFunction f{0, -1};
@@ -42,20 +42,24 @@ int main(int argc, char** argv)
 
   // Define problem
   TrialFunction u(vh);
-  // TestFunction  v(Vh);
+  TestFunction  v(vh);
 
-  // Problem elasticity(u, v);
-  // elasticity = Integral(lambda * Div(u), Div(v))
-  //            + Integral(
-  //               mu * (Jacobian(u) + Jacobian(u).T()), 0.5 * (Jacobian(v) + Jacobian(v).T()))
+  Problem elasticity(u, v);
+  elasticity = Integral(lambda * Div(u), Div(v))
+             + Integral(mu * (Jacobian(u) + Jacobian(u).T()), 0.5 * (Jacobian(v) + Jacobian(v).T()))
+             - BoundaryIntegral(f, v).over(GammaN)
+             + DirichletBC(u, VectorFunction{0, 0}).on(GammaD);
+
+  // elasticity = LinearElasticityIntegral(u, v)(lambda, mu)
   //            - BoundaryIntegral(f, v).over(GammaN)
   //            + DirichletBC(u, VectorFunction{0, 0}).on(GammaD);
-  // elasticity.solve(solver);
+
+  elasticity.solve(solver);
 
 
-  // // Save solution
-  // u.getSolution().save("u.gf");
-  // Omega.save("Omega.mesh");
+  // Save solution
+  u.getSolution().save("u.gf");
+  Omega.save("Omega.mesh");
 
   return 0;
 }
