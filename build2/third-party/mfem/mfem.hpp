e814d04d9e69567909d7f6bb6aacb6b58956abfc
Carlos Brito <carlos.brito524@gmail.com>
2023-03-06 20:33:12 +0100

    Stupid accidental addition

--- build2/third-party/mfem/mfem.hpp
+++
@@ -1,3 +0,0 @@
-// Auto-generated file.
-#define MFEM_CONFIG_FILE "/Users/carlos/Projects/rodin/build2/third-party/mfem/config/_config.hpp"
-#include "/Users/carlos/Projects/rodin/third-party/mfem/mfem.hpp"
--- build2/third-party/mmg/src/common/git_log_mmg.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GIT_LOG_MMG_H
-#define _GIT_LOG_MMG_H
-#define MMG_GIT_BRANCH "No git branch found"
-#define MMG_GIT_COMMIT "No git commit found"
-#define MMG_GIT_DATE   "No git commit found"
-#endif
