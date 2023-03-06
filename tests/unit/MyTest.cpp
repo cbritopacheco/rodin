34cfce2c50c725495ca1d3924a2308ced0dca0c1
Carlos Brito <carlos.brito524@gmail.com>
2023-03-06 18:35:33 +0100

    This time is the one

--- tests/unit/MyTest.cpp
+++ tests/unit/MyTest.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 
+#include <Rodin/Geometry.h>
+
+using namespace Rodin::Geometry;
+
 TEST(Cat, Miaow) {
-  EXPECT_EQ(1, 1);
+  Mesh mesh;
+  mesh.load("/Users/carlos/Projects/rodin/resources/mfem/StarSquare.mfem.mesh");
+  std::cout << mesh.getSpaceDimension() << std::endl;
+  EXPECT_EQ(mesh.getDimension(), 2);
 }
 
