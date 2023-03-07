5e5e0398ff9282048637e5d341d2df8897eaadde
github-action-benchmark <github@users.noreply.github.com>
2023-03-07 10:25:58 +0000

    add C++ Rodin Benchmarks (googlecpp) benchmark result for 428780c28e72fb93a089b211b832e66e13a572fe

---
+++ benchmarks/refs/heads/develop/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1678184758272,
+  "repoUrl": "https://github.com/cbritopacheco/rodin",
+  "entries": {
+    "C++ Rodin Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "carlos.brito524@gmail.com",
+            "name": "Carlos Brito-Pacheco",
+            "username": "cbritopacheco"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "428780c28e72fb93a089b211b832e66e13a572fe",
+          "message": "Update Benchmarks.yml",
+          "timestamp": "2023-03-07T11:13:41+01:00",
+          "tree_id": "b2e665ebc07e52b9f977020d233ae260c1aed5d0",
+          "url": "https://github.com/cbritopacheco/rodin/commit/428780c28e72fb93a089b211b832e66e13a572fe"
+        },
+        "date": 1678184756225,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Poisson/Assembly_ConstantCoefficient_ConstantSource",
+            "value": 401646.1675423333,
+            "unit": "ns/iter",
+            "extra": "iterations: 1713\ncpu: 394165.3239929948 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
