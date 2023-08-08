34d9e3289fea72681bc82436418b11e91581e256
Carlos Brito <carlos.brito524@gmail.com>
2023-08-07 22:03:08 -0400

    CI Errors

--- examples/Misc/RS2023/Helmholtz/Animation.cpp
+++ examples/Misc/RS2023/Helmholtz/Animation.cpp
@@ -4,7 +4,6 @@
  *       (See accompanying file LICENSE or copy at
  *          https://www.boost.org/LICENSE_1_0.txt)
  */
-#include <thread>
 #include <chrono>
 #include <fstream>
 #include <Rodin/Solver.h>
@@ -16,7 +15,7 @@ using namespace Rodin::Math;
 using namespace Rodin::Geometry;
 using namespace Rodin::Variational;
 
-static constexpr Scalar hmax = 0.005; // Maximal size of a triangle's edge
+static constexpr Scalar hmax = 0.01; // Maximal size of a triangle's edge
 static constexpr Scalar hmin = 0.1 * hmax;
 
 static constexpr Attribute dQ = 2; // Attribute of box boundary
@@ -29,8 +28,6 @@ static constexpr Scalar pi = Math::Constants::pi();
 
 static constexpr Scalar R0 = 0.2; // Radius of B_R(x_0)
 static constexpr Scalar R1 = R0 + 10 * hmax; // Radius of B_R(x_0)
-
-static std::mutex mutex_io;
 
 struct Data
 {
@@ -63,85 +60,43 @@ void run(int i, const std::vector<Data>& grid);
 int main(int, char**)
 {
   // Define evaluation grid
-  Math::Vector m_r = Math::Vector::LinSpaced(0.5 * 1. / hmax, 0, 0.5 * 1. / hmax);
+  Math::Vector m_r = Math::Vector::LinSpaced(50, 0, 0.5 * 1. / hmax);
   Math::Vector epsilon_r = Math::Vector::LinSpaced(1. / hmax, hmax, 0.2);
-  // Math::Vector waveNumber_r = Math::Vector::LinSpaced(1.0 / hmax, 1, 1.0 / hmax);
-  // Math::Vector conductivity_r{{ 1e-12, 0.5, 1.0, 2.0, 1e12 }};
-  Math::Vector waveNumber_r{{ 20, 40 }};
-  Math::Vector conductivity_r{{ 0.5, 2.0 }};
+  Math::Vector waveNumber_r = Math::Vector::LinSpaced(1.0 / hmax, 1, 1.0 / hmax);
+  Math::Vector conductivity_r{{ 1e-12, 0.5, 1.0, 2.0, 1e12 }};
 
   std::vector<Data> grid;
-  grid.reserve(m_r.size() * epsilon_r.size() * waveNumber_r.size() *  conductivity_r.size());
+  grid.reserve(m_r.size() * epsilon_r.size() * waveNumber_r.size() * conductivity_r.size());
   for (const Scalar m : m_r)
     for (const Scalar epsilon : epsilon_r)
       for (const Scalar waveNumber : waveNumber_r)
         for (const Scalar g : conductivity_r)
           grid.push_back({ m, epsilon, waveNumber, g });
 
-  const size_t hwc = std::thread::hardware_concurrency();
-  const size_t n = hwc - 2;
-  Alert::Info() << "Hardware concurrency: " << hwc
-                << Alert::NewLine
-                << "Launching " << n << " threads"
-                << Alert::Raise;
-  std::vector<std::thread> ts(n);
-  auto split = splitVector(grid, ts.size());
-
-  for (size_t i = 0; i < ts.size(); i++)
-    ts[i] = std::thread(run, i, split[i]);
-
-  for (auto& t : ts)
-    t.join();
 }
 
 void run(int id, const std::vector<Data>& grid)
 {
-  mutex_io.lock();
-  Alert::Info() << "Running thread: " << id
-                << Alert::NewLine
-                << "Grid size: " << grid.size()
-                << Alert::Raise;
-  mutex_io.unlock();
-
   // Load mesh
   Mesh mesh;
   mesh.load("Q1.medit.mesh", IO::FileFormat::MEDIT);
-  // mesh.save("out/Q.mesh");
+  mesh.save("out/Q.mesh");
 
   Solver::SparseLU solver;
 
-  // Math::Vector m_r{{  }};
-  // Math::Vector epsilon_r{{ 0.1 }};
-  // Math::Vector waveNumber_r{{ 5 }};
-  // Math::Vector conductivity_r{{ 2 }};
-
   Alert::Info() << "Grid size: " << grid.size() << Alert::Raise;
-
-  std::stringstream filename;
-  filename << "grid/T" << id << "_L2_Grid_HMax" << std::setw(4) << hmax << ".live.csv";
-  std::ofstream out(filename.str());
-  out << "m,epsilon,waveNumber,conductivity,error\n";
 
   size_t i = 0;
   auto t0 = std::chrono::high_resolution_clock::now();
   for (const auto& data : grid)
   {
-    if (i % 25 == 0)
-    {
       auto t1 = std::chrono::high_resolution_clock::now();
       const auto delta = std::chrono::duration_cast<std::chrono::seconds>(t1 - t0);
-      mutex_io.lock();
-      Alert::Info() << "Thread " << id << " progress."
-                    << Alert::NewLine
-                    << "----------------------------------"
-                    << Alert::NewLine
-                    << i << " / " << grid.size() << " ---- "
+      Alert::Info() << i << " / " << grid.size() << " ---- "
                     << (100 * Scalar(i) / Scalar(grid.size())) << "%"
                     << Alert::NewLine
                     << "Total elapsed time: " << delta.count() << "s"
                     << Alert::Raise;
-      mutex_io.unlock();
-    }
 
     // Alert::Info() << ">>>>>>>>>>>>>>>>>>>>"
     //               << Alert::NewLine
@@ -199,40 +154,33 @@ void run(int id, const std::vector<Data>& grid)
               + DirichletBC(u, ScalarFunction(0)).on(dGround);
 
     // Solve the background problem
-    // Alert::Info() << "Solving background equation." << Alert::Raise;
+    Alert::Info() << "Solving background equation." << Alert::Raise;
     helmholtz.solve(solver);
     const auto u0 = std::move(u.getSolution());
-    // Alert::Success() << "Done." << Alert::Raise;
+    Alert::Success() << "Done." << Alert::Raise;
 
-    // Alert::Info() << "Solving perturbed equation." << Alert::Raise;
+    Alert::Info() << "Solving perturbed equation." << Alert::Raise;
     perturbed.solve(solver);
     const auto ue = std::move(u.getSolution());
-    // Alert::Success() << "Done." << Alert::Raise;
+    Alert::Success() << "Done." << Alert::Raise;
+
+    ScalarFunction chi_e =
+      [&](const Point& p)
+      { return Scalar((p.getCoordinates() - x0).norm() > 2 * data.epsilon); };
 
     GridFunction diff(vh);
-    diff = Pow(u0 - ue, 2);
+    diff = chi_e * Pow(u0 - ue, 2);
     diff.setWeights();
     const Scalar error = Integral(diff);
+    Alert::Success() << "L2 Error: " << error
+                     << Alert::NewLine
+                     << "<<<<<<<<<<<<<<<<<<<<"
+                     << Alert::Raise;
 
-    // Alert::Success() << "L2 Error: " << error
-    //                  << Alert::NewLine
-    //                  << "<<<<<<<<<<<<<<<<<<<<"
-    //                  << Alert::Raise;
-    out << data.m << ','
-        << data.epsilon << ','
-        << data.waveNumber << ','
-        << data.conductivity << ','
-        << error << '\n';
-    out.flush();
-
-    // auto t1 = std::chrono::high_resolution_clock::now();
-    // auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    // Alert::Info() << "Time taken: " << delta.count() << "ms" << Alert::Raise;
-
-    // std::stringstream ss;
-    // ss << std::setfill('0') << std::setw(4) << i;
-    // u0.save("out/Background_" + ss.str() + ".gf");
-    // ue.save("out/Perturbed_" + ss.str() + ".gf");
+    std::stringstream ss;
+    ss << std::setfill('0') << std::setw(4) << i;
+    u0.save("out/Background_" + ss.str() + ".gf");
+    ue.save("out/Perturbed_" + ss.str() + ".gf");
 
     i++;
   }
@@ -240,8 +188,7 @@ void run(int id, const std::vector<Data>& grid)
   auto t1 = std::chrono::high_resolution_clock::now();
   const auto delta = std::chrono::duration_cast<std::chrono::seconds>(t1 - t0);
 
-  mutex_io.lock();
-  Alert::Success() << "Finished thread " << id << " in " << delta.count() << "s" << Alert::Raise;
-  mutex_io.unlock();
+  Alert::Success() << "Finished " << id << " in " << delta.count() << "s" << Alert::Raise;
 }
+
 
---
+++ examples/Misc/RS2023/L2_Grid_Plot.py
@@ -0,0 +1,70 @@
+from scipy.interpolate import griddata
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import argparse
+import math
+from scipy import stats
+from sklearn import preprocessing
+plt.style.use("bmh")
+
+conductivities = [2.0]
+
+x_column = 'm'
+y_column = 'epsilon'
+
+latex_label = {
+    'm' : '$m$',
+    'epsilon' : '$\epsilon$',
+    'waveNumber' : '$k$'
+}
+
+file_label = {
+    'm' : 'Period',
+    'epsilon' : 'Epsilon',
+    'waveNumber' : 'Wavenumber'
+}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str,
+                        help='Name of file to plot.')
+    args = parser.parse_args()
+    df = pd.read_csv(args.filename)
+    print('Read dataset !')
+
+    for conductivity in conductivities:
+        plt.figure()
+        print('Processing ', conductivity)
+        sdf = df[(df['conductivity'] > conductivity - 0.001) & (df['conductivity']
+                                                              < conductivity +
+                                                              0.001)]
+
+        thresh = sdf['error'].median()
+        sdf.loc[sdf['error'] > thresh, 'error'] = thresh
+
+        plt.hexbin(
+            sdf.loc[:, x_column],
+            sdf.loc[:, y_column],
+            gridsize=75,
+            cmap='inferno',
+            C=sdf.loc[:, 'error'], edgecolors='face', linewidths=0.5)
+
+        # plt.contourf(
+        #     sdf.loc[:, 'm'],
+        #     sdf.loc[:, 'epsilon'],
+        #     sdf.loc[:, 'error'])
+        plt.colorbar()
+
+        plt.xlabel(latex_label[x_column])
+        plt.ylabel(latex_label[y_column])
+        plt.title(
+            '$|| u_\epsilon - u_0 ||_{L^2 (Q / \overline{B(x, \epsilon)})} \
+            \ \mathrm{with} \ \gamma |_{B(x, e)} = %.2E$' % conductivity, pad=20)
+        out=("%s_VS_%s_Gamma=%.2E") % (file_label[x_column], file_label[y_column], conductivity)
+        plt.savefig(out + '.svg')
+        plt.savefig(out + '.png')
+        # plt.show()
+
+
+
