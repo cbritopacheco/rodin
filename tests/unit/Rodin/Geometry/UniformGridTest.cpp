b371110c24f097d53fdacc7892c29e7eeba928ec
Carlos Brito <carlos.brito524@gmail.com>
2023-11-04 22:48:41 +0100

    CI

--- tests/unit/Rodin/Geometry/UniformGridTest.cpp
+++ tests/unit/Rodin/Geometry/UniformGridTest.cpp
@@ -10,36 +10,36 @@ TEST(Rodin_Geometry_Mesh_UniformGrid, SanityTest)
   {
     Mesh mesh = SerialMesh::UniformGrid(Polytope::Type::Triangle, 2, 2);
     EXPECT_EQ(mesh.getVertexCount(), 4);
-    EXPECT_EQ(mesh.getElementCount(), 2);
+    EXPECT_EQ(mesh.getCellCount(), 2);
   }
 
   {
     Mesh mesh = SerialMesh::UniformGrid(Polytope::Type::Triangle, 2, 3);
     EXPECT_EQ(mesh.getVertexCount(), 6);
-    EXPECT_EQ(mesh.getElementCount(), 4);
+    EXPECT_EQ(mesh.getCellCount(), 4);
   }
 
   {
     Mesh mesh = SerialMesh::UniformGrid(Polytope::Type::Triangle, 3, 2);
     EXPECT_EQ(mesh.getVertexCount(), 6);
-    EXPECT_EQ(mesh.getElementCount(), 4);
+    EXPECT_EQ(mesh.getCellCount(), 4);
   }
 
   {
     Mesh mesh = SerialMesh::UniformGrid(Polytope::Type::Triangle, 5, 10);
     EXPECT_EQ(mesh.getVertexCount(), 50);
-    EXPECT_EQ(mesh.getElementCount(), 72);
+    EXPECT_EQ(mesh.getCellCount(), 72);
   }
 
   {
     Mesh mesh = SerialMesh::UniformGrid(Polytope::Type::Triangle, 4, 9);
     EXPECT_EQ(mesh.getVertexCount(), 36);
-    EXPECT_EQ(mesh.getElementCount(), 48);
+    EXPECT_EQ(mesh.getCellCount(), 48);
   }
 
   {
     Mesh mesh = SerialMesh::UniformGrid(Polytope::Type::Triangle, 16, 16);
     EXPECT_EQ(mesh.getVertexCount(), 256);
-    EXPECT_EQ(mesh.getElementCount(), 450);
+    EXPECT_EQ(mesh.getCellCount(), 450);
   }
 }
