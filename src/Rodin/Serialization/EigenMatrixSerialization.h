1e49a6a469e6a44e7173af59c2ed1a161da6f353
Carlos Brito <carlos.brito524@gmail.com>
2023-12-12 14:48:49 +0100

    CI

--- src/Rodin/Serialization/EigenMatrixSerialization.h
+++ src/Rodin/Serialization/EigenMatrixSerialization.h
@@ -3,32 +3,22 @@
 
 #include <Eigen/Core>
 
+#include <boost/serialization/array.hpp>
+
 namespace boost::serialization
 {
   template <class Archive, typename Derived, int Rows, int Cols, int Options, int MaxRows, int MaxCols>
   inline
   void serialize(
       Archive & ar,
-      Eigen::Matrix<Derived, Rows, Cols, Options, MaxRows, MaxCols> & matrix,
+      Eigen::Matrix<Derived, Rows, Cols, Options, MaxRows, MaxCols>& matrix,
       const unsigned int version)
   {
     const Eigen::Index rows = matrix.rows();
     const Eigen::Index cols = matrix.cols();
     ar & rows;
     ar & cols;
-    if (Archive::is_saving::value)
-    {
-      for (Eigen::Index i = 0; i < rows; ++i)
-        for (Eigen::Index j = 0; j < cols; ++j)
-          ar & matrix(i, j);
-    }
-    else
-    {
-      matrix.resize(rows, cols);
-      for (Eigen::Index i = 0; i < rows; ++i)
-        for (Eigen::Index j = 0; j < cols; ++j)
-          ar & matrix(i, j);
-    }
+    ar & boost::serialization::make_array(matrix.data(), matrix.size());
   }
 }
 
