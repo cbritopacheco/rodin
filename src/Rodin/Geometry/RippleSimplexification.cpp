e9f47b508732e7cb93efd8c53e7769de49756d97
Carlos Brito <carlos.brito524@gmail.com>
2025-02-04 09:40:00 +0100

    Work on simplexification

--- src/Rodin/Geometry/RippleSimplexification.cpp
+++ src/Rodin/Geometry/RippleSimplexification.cpp
@@ -16,9 +16,16 @@ namespace Rodin::Geometry
       Polytope::Type::Quadrilateral,
       {
         { {{0, 1, 2}}, {{1, 2, 3}} },
-        { {{0, 1, 2}}, {{0, 2, 3}} }
+        { {{0, 1, 3}}, {{0, 2, 3}} }
       }
-    }
+    },
+    {
+      Polytope::Type::TriangularPrism,
+      {
+        { {{0, 1, 2}}, {{1, 2, 3}} },
+        { {{0, 1, 3}}, {{0, 2, 3}} }
+      }
+    },
   };
 
   RippleSimplexification<Mesh<Context::Local>>
@@ -41,10 +48,11 @@ namespace Rodin::Geometry
     {
       const Index i = searchQueue.back();
       searchQueue.pop_back();
+      visitedCount[i] += 1;
       auto it = mesh.getFace(i);
 
       // Check if the face triangulation is compatible with the adjacent cells
-      bool compatible = true;
+      bool compatible;
 
       if (it->getGeometry() == Polytope::Type::Triangle)
         continue;
@@ -60,12 +68,13 @@ namespace Rodin::Geometry
         {
           case Polytope::Type::TriangularPrism:
           {
+            compatible = false;
             const auto& faces = mesh.getConnectivity().getIncidence({D, D - 1}, j);
             for (const Index k : faces)
             {
               const auto& triangulation = m_triangulation[k];
               const auto& vs = mesh.getConnectivity().getPolytope(D - 1, k);
-              const auto& simplices = s_simplices[it->getGeometry()].at(triangulation);
+              const auto& simplices = s_simplices[mesh.getGeometry(D - 1, k)].at(triangulation);
               for (const auto& simplex : simplices)
               {
                 for (const Index sv : simplex)
@@ -75,9 +84,13 @@ namespace Rodin::Geometry
 
             for (const auto& [k, v] : count)
             {
-              std::cout << "k: " << k << ", v: " << v << std::endl;
+              // One vertex must be shared by at least 5 triangles
+              if (v >= 5)
+              {
+                compatible = true;
+                break;
+              }
             }
-            std::exit(1);
             break;
           }
         }
@@ -86,18 +99,40 @@ namespace Rodin::Geometry
           break;
       }
 
-      // If not compatible, then change the triangulation for this face
+      // If not compatible, then change the triangulation and add the faces in
+      // the incident cells to the search queue
       if (!compatible)
       {
         m_triangulation[i] = visitedCount[i] % s_simplices[it->getGeometry()].size();
-        for (auto adj = it->getAdjacent(); adj; ++adj)
+        for (const auto& j : cells)
         {
-          const Index j = adj->getIndex();
-          searchQueue.push_back(j);
-
+          auto cell = mesh.getCell(j);
+          const auto& faces = mesh.getConnectivity().getIncidence({D, D - 1}, j);
+          for (const Index k : faces)
+          {
+            if (k == i)
+              continue;
+            searchQueue.push_back(k);
+          }
         }
       }
     }
+
+    // Build the new mesh
+    for (auto it = mesh.getCell(); it; ++it)
+    {
+      const auto& cell = *it;
+      const auto& faces = mesh.getConnectivity().getIncidence({D, D - 1}, cell.getIndex());
+      for (const Index i : faces)
+      {
+        const auto& triangulation = m_triangulation[i];
+        const auto& simplices = s_simplices[mesh.getGeometry(D - 1, i)].at(triangulation);
+        for (const auto& simplex : simplices)
+        {
+        }
+      }
+    }
+
     return m_build.finalize();
   }
 }
