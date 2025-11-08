1ebfe814afc41ab13fab675705bea27cec0bc8aa
Carlos Brito <carlos.brito524@gmail.com>
2025-11-08 15:17:50 +0100

    CI

--- src/Rodin/Serialization/UnorderedMap.h
+++ src/Rodin/Serialization/UnorderedMap.h
@@ -1,13 +1,20 @@
 #ifndef RODIN_SERIALIZATION_UNORDEREDMAP_H
 #define RODIN_SERIALIZATION_UNORDEREDMAP_H
 
-#include <boost/unordered_map.hpp>
+// Must come first so library_version_type is visible to Boost helpers
+#include <boost/serialization/library_version_type.hpp>
 #include <boost/serialization/version.hpp>
+
+// Legacy containers: boost::unordered_map (namespace boost)
+#include <boost/unordered_map.hpp>
 #include <boost/serialization/unordered_map.hpp>
 
-#if BOOST_VERSION < 108400
-#include <boost/serialization/boost_unordered_map.hpp>
+// New containers: boost::unordered::unordered_map (namespace boost::unordered)
+#include <boost/unordered/unordered_map.hpp>
+
+// Since Boost 1.84, serialization for the *new* containers lives here:
+#if BOOST_VERSION >= 108400
+  #include <boost/serialization/boost_unordered_map.hpp>
 #endif
 
-#endif
-
+#endif // RODIN_SERIALIZATION_UNORDEREDMAP_H
